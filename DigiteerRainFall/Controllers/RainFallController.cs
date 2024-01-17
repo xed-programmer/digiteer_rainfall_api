ff875fd2d2b833428fb89effe0099b4e4f1e2607
xed-programmer <xedprogrammer@gmail.com>
2024-01-17 21:55:54 +0800

    add httpclient for calling api, pero ma error pato

--- DigiteerRainFall/Controllers/RainFallController.cs
+++ DigiteerRainFall/Controllers/RainFallController.cs
@@ -1,4 +1,6 @@
 using Microsoft.AspNetCore.Mvc;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace DigiteerRainFall.Controllers;
 
@@ -6,9 +8,18 @@ namespace DigiteerRainFall.Controllers;
 [Route("[controller]")]
 public class RainFallController : ControllerBase
 {
-    [HttpGet("/id/{stationId}/readings")]
-    public IActionResult GetStationReadings(string stationId)
+    private readonly HttpClient _httpClient;
+
+    public RainFallController(IHttpClientFactory httpClientFactory)
     {
-        return Ok(stationId);
+        _httpClient = httpClientFactory.CreateClient();
+        _httpClient.BaseAddress = new System.Uri("http://environment.data.gov.uk/flood-monitoring");
+    }
+
+    [HttpGet("/id/{stationId}/readings")]
+    public async Task<IActionResult> GetStationReadings(string stationId)
+    {
+        var response = await _httpClient.GetAsync("id/stations/1491TH/readings");
+        return Ok(response);
     }
 }
